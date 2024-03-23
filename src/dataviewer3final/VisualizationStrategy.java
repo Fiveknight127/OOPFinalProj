e7c02e17463a3c9644eb090751081c34e74ca1cd
Seantastik <spothekid@gmail.com>
2024-03-22 21:19:30 -0500

    ok

--- src/dataviewer3final/VisualizationStrategy.java
+++ src/dataviewer3final/VisualizationStrategy.java
@@ -1,8 +1,9 @@
 package dataviewer3final;
 import java.awt.Color;
 public class VisualizationStrategy {
+	
+	
     private boolean extremaVisualization = false;
-
 	    
 		public boolean execute() {
 			extremaVisualization = false;
